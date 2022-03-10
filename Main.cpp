40dd463ee1d1bcd7e0bafecfdc73c34897411b93
Yehudit-Brickner <nech.diti@yahoo.com>
2022-03-10 10:59:11 +0200

    added more to main

--- Main.cpp
+++ Main.cpp
@@ -28,20 +28,68 @@ int main(){
     // char c= distr(eng);
     // cout << c<< endl;
 
-    int nums[] ={1,3,5,7,11,13};
+    int nums1[] ={1,3,5};
     
-    for (int i=0; i<5;i++){
-        for (int j=0; j<5;j++){
+    for (int i=0; i<3;i++){
+        for (int j=0; j<3;j++){
             char c= distr(eng);
             char d= distr(eng);
             try{
-            std::cout << ariel::mat(nums[i],nums[j],c,d) <<endl; 
+                cout<< "rows  "<< nums1[j]<< "  columns  " << nums1[i]<<endl;
+                cout << ariel::mat(nums1[i],nums1[j],c,d) <<endl; 
             }
             catch (exception& ex) {
 	 	        cout << "   caught exception: " << ex.what() << endl;  
 	        }
         }
     } 
+
+    int nums2[] ={13,23,85};
+    for (int i=0; i<3;i++){
+        for (int j=0; j<3;j++){
+            char c= distr(eng);
+            char d= distr(eng);
+            try{
+                cout<< "rows  "<< nums2[j]<< "  columns  " << nums2[i]<<endl;
+                cout << ariel::mat(nums2[i],nums2[j],c,d) <<endl; 
+            }
+            catch (exception& ex) {
+	 	        cout << "   caught exception: " << ex.what() << endl;  
+	        }
+        }
+    } 
+
+            try{
+                cout<< "rows  "<< 4<< "  columns  " << 7<<endl;
+                cout << ariel::mat(7,4,'#','/') <<endl; 
+            }
+            catch (exception& ex) {
+	 	        cout << "   caught exception: " << ex.what() << endl;  
+	        }
+            try{
+                cout<< "rows  "<< 0<< "  columns  " << 7<<endl;
+                cout << ariel::mat(7,0,'#','/') <<endl; 
+            }
+            catch (exception& ex) {
+	 	        cout << "   caught exception: " << ex.what() << endl;  
+	        }
+            try{
+                cout<< "rows  "<< -5<< "  columns  " << 7<<endl;
+                cout << ariel::mat(7,-5,'#','/') <<endl; 
+            }
+            catch (exception& ex) {
+	 	        cout << "   caught exception: " << ex.what() << endl;  
+	        }
+             try{
+                cout<< "rows  "<< 7<< "  columns  " << 7<<endl;
+                cout << ariel::mat(7,7,' ','/') <<endl; 
+            }
+            catch (exception& ex) {
+	 	        cout << "   caught exception: " << ex.what() << endl;  
+	        }
+
+
+
    //chars 32-126
 //   cout << "i hate c++"<< endl;
 //   std::cout << ariel::mat(5,3,'*','=') <<endl;
