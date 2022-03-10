13e445821c38b2f33587cdc008571ec9ff6b4546
Yehudit-Brickner <nech.diti@yahoo.com>
2022-03-10 10:32:22 +0200

    starting creating the main

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdexcept>
+// #include <ctime>
+#include <random>
 // #include <string>
 // #include <vector>
 
@@ -15,10 +17,36 @@ namespace ariel{
 
 
 int main(){   
+
+    // srand((unsigned) time(0));
+    // int result =(rand());
+    // std::cout<< result % 126 << endl;
+
+    std::random_device rd;
+    std::default_random_engine eng(rd());
+    std::uniform_int_distribution<int> distr(32, 126);
+    // char c= distr(eng);
+    // cout << c<< endl;
+
+    int nums[] ={1,3,5,7,11,13};
+    
+    for (int i=0; i<5;i++){
+        for (int j=0; j<5;j++){
+            char c= distr(eng);
+            char d= distr(eng);
+            try{
+            std::cout << ariel::mat(nums[i],nums[j],c,d) <<endl; 
+            }
+            catch (exception& ex) {
+	 	        cout << "   caught exception: " << ex.what() << endl;  
+	        }
+        }
+    } 
+   //chars 32-126
 //   cout << "i hate c++"<< endl;
-  std::cout << ariel::mat(5,3,'-','+') <<endl;
-string s= ariel::mat(111,57,'*','/');
-    std::cout << s<<"\n"<< endl;
+//   std::cout << ariel::mat(5,3,'*','=') <<endl;
+// string s= ariel::mat(111,57,'*','/');
+//     std::cout << s<<"\n"<< endl;
 //    s= mat(3,3,'+','-'); 
 //     cout << s<<" \n"<< endl;
 //   s=  mat(1,1,'^','-'); 
@@ -41,7 +69,7 @@ string s= ariel::mat(111,57,'*','/');
 //     s=mat(47,25,'*','$');
 //      cout << s<<" \n"<< endl;
 //     s=mat(25,47,'*','$');
-//      cout << s<<" \n"<< endl;
+//      cou-t << s<<" \n"<< endl;
 //     s=mat(77, 11, '-', '+');
 //      cout << s<<" \n"<< endl;
 //    s= mat(15, 83, '!', '@');
