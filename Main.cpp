8863d33ddc1bccc8a3b41e6ba08dbc2e481f8703
Yehudit-Brickner <nech.diti@yahoo.com>
2022-03-11 15:28:42 +0200

    added comments to main

--- Main.cpp
+++ Main.cpp
@@ -4,29 +4,27 @@
 #include <random>
 // #include <string>
 // #include <vector>
-
+#include "mat.hpp"
 using namespace std;
 // using namespace ariel;
 
-namespace ariel{
-    string mat(int a, int b, char c, char d);
-	string matreg(int a, int b, char c, char d);
-    string matlong(int a, int b, char c, char d);
-    string matsame(int a, int b, char c);
-}
+// namespace ariel{
+//     string mat(int columns, int rows, char symb1, char symb2);
+// 	string matreg(int columns, int rows, char symb1, char symb2);
+//     string matlong(int columns, int rows, char symb1, char symb2);
+//     string matsame(int columns, int rows, char symb1);
+// }
 
 
 int main(){   
 
-    // srand((unsigned) time(0));
-    // int result =(rand());
-    // std::cout<< result % 126 << endl;
+    
 
     std::random_device rd;
     std::default_random_engine eng(rd());
     std::uniform_int_distribution<int> distr(32, 126);
-    // char c= distr(eng);
-    // cout << c<< endl;
+    char c= distr(eng);
+    cout << c<< endl;
 
     int nums1[] ={1,3,5};
     
@@ -61,67 +59,47 @@ int main(){
 
             try{
                 cout<< "rows  "<< 4<< "  columns  " << 7<<endl;
-                cout << ariel::mat(7,4,'#','/') <<endl; 
+                cout << ariel::mat(7,4,'#','/') <<endl;  // contains even should throw an error
             }
             catch (exception& ex) {
 	 	        cout << "   caught exception: " << ex.what() << endl;  
 	        }
             try{
                 cout<< "rows  "<< 0<< "  columns  " << 7<<endl;
-                cout << ariel::mat(7,0,'#','/') <<endl; 
+                cout << ariel::mat(7,0,'#','/') <<endl; // contains 0 should throw an error
             }
             catch (exception& ex) {
 	 	        cout << "   caught exception: " << ex.what() << endl;  
 	        }
             try{
                 cout<< "rows  "<< -5<< "  columns  " << 7<<endl;
-                cout << ariel::mat(7,-5,'#','/') <<endl; 
+                cout << ariel::mat(7,-5,'#','/') <<endl; // contains negative should throw an error
             }
             catch (exception& ex) {
 	 	        cout << "   caught exception: " << ex.what() << endl;  
 	        }
              try{
                 cout<< "rows  "<< 7<< "  columns  " << 7<<endl;
-                cout << ariel::mat(7,7,' ','/') <<endl; 
+                cout << ariel::mat(7,7,' ','/') <<endl; // contains illegal symbol should throw an error
             }
             catch (exception& ex) {
 	 	        cout << "   caught exception: " << ex.what() << endl;  
 	        }
 
+            int row ='a';
+            int col = 'c';
+            cout<< "rows  "<< row << "  columns  " << col <<endl;
+            cout << ariel::mat(col,row,'*','/') <<endl; 
+            
 
 
-   //chars 32-126
-//   cout << "i hate c++"<< endl;
-//   std::cout << ariel::mat(5,3,'*','=') <<endl;
-// string s= ariel::mat(111,57,'*','/');
-//     std::cout << s<<"\n"<< endl;
-//    s= mat(3,3,'+','-'); 
-//     cout << s<<" \n"<< endl;
-//   s=  mat(1,1,'^','-'); 
-//     cout << s<<" \n"<< endl;
-//  s=   mat(1,3,'&','*'); 
-//     cout << s<<" \n"<< endl;
-//    s= mat(3,1,'$','+'); 
-//     cout << s<<"\n "<< endl; 
-//    s= mat(7,3,'/','-'); 
-//     cout <<s<< "\n "<< endl;
-//    s= mat(7,9,'@','*'); 
-//     cout << s<<" \n"<< endl;
-//   s=  mat(5,11,'@','*'); 
-//     cout <<s<< " \n"<< endl;
-//    s= mat(7,21,'@','*'); 
-//     cout << s<<" \n"<< endl;
-//    s= mat(11,5,'*','*'); 
-//     cout << s<<" \n"<< endl;
+         
+
+
+
+
    
-//     s=mat(47,25,'*','$');
-//      cout << s<<" \n"<< endl;
-//     s=mat(25,47,'*','$');
-//      cou-t << s<<" \n"<< endl;
-//     s=mat(77, 11, '-', '+');
-//      cout << s<<" \n"<< endl;
-//    s= mat(15, 83, '!', '@');
-//      cout << s<<" \n"<< endl;
+
 
 
     return 0;
